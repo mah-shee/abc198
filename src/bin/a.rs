f105a649c4d0b108ca1a11e23727db742810d91a
masa <masatoshi.sboard@gmail.com>
2021-04-11 21:21:10 +0900

    done a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,5 +4,11 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+    }
+    if n <= 1 {
+        println!("0");
+    } else {
+        println!("{}", n - 1);
     }
 }
