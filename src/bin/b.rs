58a776dec8efc9db03562ff5213f06c86b3308c6
masa <masatoshi.sboard@gmail.com>
2021-04-11 21:44:26 +0900

    done b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -4,5 +4,26 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: Chars,
     }
+    let a = n.len();
+    // nからtrailinng_zeroを除き、回文が成立するかを調べる。
+    let mut cnt_trailling_zero = 0;
+    for i in n.iter().rev() {
+        if i == &'0' {
+            cnt_trailling_zero += 1;
+        } else {
+            break;
+        }
+    }
+    let len = a - cnt_trailling_zero;
+    for i in 0..len / 2 {
+        if n[i] == n[len - 1 - i] {
+            continue;
+        } else {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
