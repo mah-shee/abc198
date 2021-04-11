170e5debadd78a0a01409bcde24638a50b6ca9d5
masa <masatoshi.sboard@gmail.com>
2021-04-11 22:05:53 +0900

    done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,12 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        r: usize,
+        x: usize,
+        y: usize
     }
+    // 頂点からの距離 / 半径
+    let distance = (x.pow(2) + y.pow(2)) as f64;
+    let radius = r.pow(2) as f64;
+    println!("{}", ((distance / radius) as f64).sqrt().ceil() as usize);
 }
