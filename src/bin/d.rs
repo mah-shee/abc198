f445a48d13b43f970b17133277464b925932c1cb
masa <masatoshi.sboard@gmail.com>
2021-04-24 20:59:38 +0900

    done contest

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -4,5 +4,12 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        a: Chars,
+        b: Chars,
+        c: Chars,
     }
+    // 0~9をそれぞれのアルファベットに当てはめるので, 10文字以上出てきたら解けない
+    // 各文字に0~9をそれぞれ当てはめてゴリゴリ回す
+
 }
+fn solve()
